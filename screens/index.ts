680378d7c21d756e365744f4d03ea9738769a9b0
alejandroquadri <ale@quadri.com.ar>
2025-09-02 22:42:38 -0300

    refactoring

--- screens/index.ts
+++ screens/index.ts
@@ -3,5 +3,5 @@ export * from './shared';
 export * from './chat-screen';
 export * from './progress-screen';
 export * from './log-screen';
-export * from './profile-screen';
+export * from './profile-stack';
 export * from './splash-screen';
