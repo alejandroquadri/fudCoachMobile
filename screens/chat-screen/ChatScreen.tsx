f13772454160a5a162b82c4cf58e41f3307ab7ab
alejandroquadri <ale@quadri.com.ar>
2026-09-18 15:27:59 -0300

    Stabilize chat keyboard offset

--- screens/chat-screen/ChatScreen.tsx
+++ screens/chat-screen/ChatScreen.tsx
@@ -12,6 +12,7 @@ import {
 import 'react-native-get-random-values';
 import { GiftedChat, IMessage, Send } from 'react-native-gifted-chat';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useHeaderHeight } from '@react-navigation/elements';
 
 import { COLORS } from '@theme';
 import { ChatStyles } from './ChatStyles';
@@ -66,6 +67,7 @@ export const Chat = () => {
   const [showDialogSettings, setShowDialogSettings] = useState(false);
 
   const insets = useSafeAreaInsets();
+  const headerHeight = useHeaderHeight();
   const styles = ChatStyles(insets.bottom);
 
   const { user } = useAuth();
@@ -268,7 +270,7 @@ export const Chat = () => {
         renderSend={renderSend}
         isSendButtonAlwaysVisible={true}
         keyboardAvoidingViewProps={{
-          automaticOffset: true,
+          keyboardVerticalOffset: headerHeight,
         }}
       />
 
